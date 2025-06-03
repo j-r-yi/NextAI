f9d4ec703ec8a5e87985aa3df50674c62cf38d1d
Joshua Yi <jryi@scu.edu>
2025-06-02 20:41:56 -0700

    server.js implement apis

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
+// Takes care of storing pdf when user upload PDF
 const storage = multer.diskStorage({
   // Where to store file
   destination: function (req, file, cb) {
@@ -23,4 +24,28 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
+});
+
+const PORT = 5001;
+
+let filePath;
+
+// POST Request Handler
+app.post('/upload', upload.single('file'), async (req, res) => {
+  // Should check if pdf file
+  filePath = req.file.path;
+  // Sends to frontend
+  res.send(filePath + 'upload successful');
+});
+
+// GET Request Handler
+app.get('/chat', async (req, res) => {
+  const resp = await chat(filePath, req.query.question);
+  res.send(resp.text);
+  //   console.log('get chat request received');
+});
+
+// Server Running
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
 });
