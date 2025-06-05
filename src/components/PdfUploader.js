f37714895ea457ad1cbff7e667d9f76ad7ffc0f2
Joshua Yi <jryi@scu.edu>
2025-06-04 20:04:34 -0700

    implement PdfUploader component

---
+++ src/components/PdfUploader.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import axios from 'axios';
+import { InboxOutlined } from '@ant-design/icons';
+import { message, Upload } from 'antd';
+
+const { Dragger } = Upload;
+
+const DOMAIN = process.env.REACT_APP_DOMAIN;
+
+const uploadToBackend = async (file) => {
+  const formData = new FormData();
+  formData.append('file', file);
+  try {
+    const response = await axios.post(`${DOMAIN}/upload`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    return response;
+  } catch (err) {
+  } finally {
+    return null;
+  }
+};
+
+// Attributes to pass to antd dragger later
+const attributes = {
+  name: 'file',
+  // Allow multiple uploads at once? Code only accepts first pdf
+  multiple: false,
+  customRequest: async ({ file, onSuccess, onError }) => {
+    const response = await uploadToBackend(file);
+    if (response && response.status === 200) {
+      onSuccess(response.data);
+    } else {
+      onError(new Error('upload failed'));
+    }
+  },
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log('Dropped files', e.dataTransfer.files);
+  },
+};
+
+const PdfUploader = () => {
+  return (
+    <Dragger {...attributes}>
+      <p className="ant-upload-drag-icon">
+        <InboxOutlined />
+      </p>
+      <p className="ant-upload-text">
+        Click or drag file to this area to upload
+      </p>
+      <p className="ant-upload-hint">
+        Support for a single upload only. Strictly prohibited from uploading
+        company data or other sensitive files.
+      </p>
+    </Dragger>
+  );
+};
